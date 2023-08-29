ffd62cb7ab8f85f7442f7733f5a0f8c603ab9731
Mudit <muditert34@gmail.com>
2023-08-29 15:37:41 +0530

    added jwt and bcrypt

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,36 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
+dotenv.config();
 
 const ADMIN = [];
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     if (ADMIN.find(user => user.username === username)) {
-        res.status(401).json('user already exists');
+        res.status(403).json('User already exists');
     } else {
-        ADMIN.push({ username, password });
-        res.status(200).json({ username, password });
+        ADMIN.push({ username, password: hashedPassword });
+        res.status(200).json({ username });
     }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = ADMIN.find(user => user.username === username && user.password === password);
+    const admin = ADMIN.find(admin => admin.username === username);
 
-    if (user) {
-        res.status(200).json('Login successful');
+    if (admin && await bcrypt.compare(password, admin.password)) {
+        const token = jwt.sign({ username, role: 'admin' }, process.env.SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Logged in successfully', token });
     } else {
-        res.status(401).json('Wrong credentials');
+        res.status(403).json('Wrong credentials');
     }
-})
+});
 
 module.exports = router;
+
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,36 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
+dotenv.config();
 
 const USER = [];
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     if (USER.find(user => user.username === username)) {
-        res.status(401).json('user already exists');
+        res.status(403).json('User already exists');
     } else {
-        USER.push({ username, password });
-        res.status(200).json({ username, password });
+        USER.push({ username, password: hashedPassword });
+        res.status(200).json({ username });
     }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = USER.find(user => user.username === username && user.password === password);
-    if (user) {
-        res.status(200).json('Login successful');
+    const user = USER.find(user => user.username === username);
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username, role: 'user' }, process.env.SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Logged in successfully', token });
     } else {
-        res.status(401).json('Wrong credentials');
+        res.status(403).json('Invalid credentials');
     }
-})
+});
+
 
 module.exports = router;
